6656a2cc634df72a48f7bf456e723a68b78e9fe1
“eliabedossantos” <eliabe.s.o36@gmail.com>
2022-09-25 00:10:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({ error: "Customer not found!" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
+app.post("/account", (request, response) => {
+    const { name, cpf, email} = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    customers.push({
+        id: uuidv4(),
+        name,
+        cpf,
+        email,
+        statement: []
+    });
+    return response.status(201).send();
+});
+
+// app.use(verifyIfExistsAccountCPF);
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) =>{
+    const { customer } = request;
+
+    return response.json(customer.statement);
+});
+
+
+
+app.listen(3333); // localhost:3333
